1eab5550dcf7ff94a2cc2133f09a505ed2a4a222
aditya <aditdualapan@gmail.com>
2022-01-30 11:15:10 +0700

    up

--- pages/api/test.js
+++ pages/api/test.js
@@ -3,18 +3,22 @@ import cheerio from "cheerio";
 
 export default async function Test(req, res)
 {
-    const plain = await axios.get('https://komikindo.id/daftar-komik/page/2')
-    const response = await plain.data;
-    const $ = cheerio.load(response);
-    const arr = []
-    const file = $('.animepost')
-    .children()
-    .map(function()
-    {
-      const obj ={name:"", poster:""}
-      obj.name =  $(this).find('h4').text();
-      obj.poster = $(this).find('img').attr('src');
-      arr.push(obj);
-    })
-    res.send(arr)
+    try {
+        const plain = await axios.get('https://komikindo.id/daftar-komik/page/2')
+        const response = await plain.data;
+        const $ = cheerio.load(response);
+        const arr = []
+        const file = $('.animepost')
+        .children()
+        .map(function()
+        {
+          const obj ={name:"", poster:""}
+          obj.name =  $(this).find('h4').text();
+          obj.poster = $(this).find('img').attr('src');
+          arr.push(obj);
+        })
+        res.status(200).json(JSON.stringify(arr));        
+    } catch (error) {
+        res.send(error)
+    }
 }
